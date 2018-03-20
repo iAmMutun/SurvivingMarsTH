b2318bf64864318af96c4af3c3d48c91cb49ba2c
Theeramet Ratsameewijitpaisan <i.am.mutun@gmail.com>
2018-03-21 00:06:00 +0700

    Upload font change to Workshop.

--- Thai Translations/metadata.lua
+++ Thai Translations/metadata.lua
@@ -1,12 +1,12 @@
 return PlaceObj('ModDef', {
 	'title', "Thai Localization Pack",
-	'description', "ม็อดแปลภาษาไทย\r\n\r\nม็อดยังอยู่ระหว่างการทดสอบ เนื่องจากภายในเกมไม่มีฟอนต์ภาษาไทย การติดตั้งม็อดจากสตีมโดยตรงจึงยังไม่สามารถใช้งานได้ทั้งหมด\r\n\r\nถ้าคุณพร้อมรับความเสี่ยงเพื่อให้ได้เล่นภาษาไทยก่อนใคร อ่านวิธีการดาวน์โหลดและติดตั้งไฟล์เพิ่มเติมที่ https://github.com/iAmMutun/SurvivingMarsTH/releases/tag/dep1\r\n\r\nติดตามอัพเดทเพิ่มเติมที่ https://github.com/iAmMutun/SurvivingMarsTH\r\n\r\nDISCLAIMER\r\nThis mod required additional modification to the original game files in order to works. Such modification has been listed in details in Thai at https://github.com/iAmMutun/SurvivingMarsTH/releases/tag/dep1, along with a similar warning to this disclaimer. Players are subject to their own decision to use this mod. Neither the mod creators nor the game developer are responsible in the event that the mod cause any issue to the game.",
+	'description', "ม็อดแปลภาษาไทย\r\n\r\nม็อดยังอยู่ระหว่างการทดสอบ เนื่องจากภายในเกมไม่มีฟอนต์ภาษาไทย การติดตั้งม็อดจากสตีมโดยตรงจึงยังไม่สามารถใช้งานได้ทั้งหมด\r\n\r\nถ้าคุณพร้อมรับความเสี่ยงเพื่อให้ได้เล่นภาษาไทยก่อนใคร อ่านวิธีการดาวน์โหลดและติดตั้งไฟล์เพิ่มเติมที่ https://github.com/iAmMutun/SurvivingMarsTH/releases/\r\n\r\nติดตามอัพเดทเพิ่มเติมที่ https://github.com/iAmMutun/SurvivingMarsTH\r\n\r\nDISCLAIMER\r\nThis mod required additional modification to the original game files in order to works. Such modification has been listed in details in Thai at https://github.com/iAmMutun/SurvivingMarsTH/releases/, along with a similar warning to this disclaimer. Players are subject to their own decision to use this mod. Neither the mod creators nor the game developer are responsible in the event that the mod cause any issue to the game.",
 	'tags', "Localization",
 	'image', "thai.png",
 	'id', "BjELS2W",
 	'steam_id', "1335965052",
 	'author', "i.am.mutun",
-	'version', 25,
+	'version', 26,
 	'lua_revision', 228003,
 	'loctables', {
 		{
@@ -14,5 +14,5 @@ return PlaceObj('ModDef', {
 			language = "Thai",
 		},
 	},
-	'saved', 1521563337,
+	'saved', 1521565535,
 })
